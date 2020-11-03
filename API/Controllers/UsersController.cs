1aa72975a61cf9ecffdf0ea34299392eefa09b72
Jesus <jesus.perez.dev@gmail.com>
2020-11-02 19:22:47 -0500

    Base API

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+        
+        public async Task<AppUser> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
